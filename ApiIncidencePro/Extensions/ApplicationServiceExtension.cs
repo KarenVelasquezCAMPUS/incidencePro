7564b98ee365fe5b119d19b7ef4e37a91c9884d2
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-09-05 12:45:26 -0500

    feat: :sparkles: Extensions

---
+++ ApiIncidencePro/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace ApiIncidencePro.Extensions;
+public static class ApplicationServiceExtension
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+    services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+    });
+}
