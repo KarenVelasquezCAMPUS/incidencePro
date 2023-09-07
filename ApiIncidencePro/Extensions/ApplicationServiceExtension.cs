071cf95136ad97be285355411d9602cc41650993
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-09-07 08:06:50 -0500

    feat: :zap: Implementacion Metodo Extension en la Clase
    para el Rate Limiting, pero no sirve todavia la peticion get a la hora de hacer la consulta en el thunder

--- ApiIncidencePro/Extensions/ApplicationServiceExtension.cs
+++ ApiIncidencePro/Extensions/ApplicationServiceExtension.cs
@@ -1,4 +1,5 @@
 using Application.UnitOfWork;
+using AspNetCoreRateLimit;
 using Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
@@ -19,5 +20,28 @@ public static class ApplicationServiceExtension
         //services.AddScoped<IAreaInterface, AreaRepository>();
         //services.AddScoped<IAreaUser, AreaUserRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+    public static void ConfigureRatelimiting(this IServiceCollection services)
+    {
+        services.AddMemoryCache();
+        services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        services.AddInMemoryRateLimiting();
+        services.Configure<IpRateLimitOptions>(options =>
+        {
+            options.EnableEndpointRateLimiting = true;
+            options.StackBlockedRequests = false;
+            options.HttpStatusCode = 429;
+            options.RealIpHeader = "X-Real-IP";
+            options.GeneralRules = new List<RateLimitRule>
+            {
+                new RateLimitRule
+                {
+                    Endpoint = "*",
+                    Period = "10s",
+                    Limit = 2
+                }
+            };
+
+        });
     }
 }
